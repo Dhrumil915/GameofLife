34b590fce89c5ad212571257a96a816ee769431d
Dhrumil915 <panchald915@gmail.com>
2022-08-19 10:54:27 +0530

    GameofLife

---
+++ src/com/game/Game2.java
@@ -0,0 +1,46 @@
+package com.game;
+
+import java.util.Scanner;
+
+public class Game2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+			System.out.println("\n*** Welcome to the Game of Life! ***\n");
+			System.out.print("Give the grid's height: ");
+			int height = scan.nextInt();
+			System.out.print("\nGive the grid's width: ");
+			int width = scan.nextInt();
+			while (height < 1 || width < 1) {
+				System.out.println("\nGrid dimensions should be greater than 0.");
+				System.out.print("\nHeight: ");
+				height = scan.nextInt();
+				System.out.print("\nWidth: ");
+				width = scan.nextInt();
+			}
+			System.out.print("\nGive the number of generations: ");
+			int generations = scan.nextInt();
+			while (generations < 1) {
+				System.out.println("\nNumber of generations should be greater than 0.");
+				System.out.print("\nNumber of generations: ");
+				generations = scan.nextInt();
+			}
+			Game1 gol = new Game1(height, width);
+			System.out.println("\nThe initial grid:");
+			gol.printGrid();
+			for (int i = 1; i <= generations; i++) {
+				gol.nextGeneration();
+				System.out.println("\nGeneration " + i + ":");
+				gol.printGrid();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("\n*** End of the Game of Life! ***\n\n");
+			scan.close();
+
+	}
+
+}
